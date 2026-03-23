5e2acd7c905c1842bc033cc217904387708a28ad
Ciol894 <loic.corvalan@wsp.com>
2026-03-23 17:55:35 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-voila app.ipynb --port=$PORT --no-browser --strip_sources=False --Voila.ip=0.0.0.0
+voila RE2020.ipynb --port=$PORT --no-browser --strip_sources=False --Voila.ip=0.0.0.0
